f29c63d1bb5416727e2e8551b1d3af4ee5c55613
yuichi.imai <yuichi.imai.1403@gmail.com>
2017-07-02 12:42:14 +0900

    HPソースサンプル追加

---
+++ src/main/java/com/uroc88/eclair/EclairApplication.java
@@ -0,0 +1,12 @@
+package com.uroc88.eclair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclairApplication.class, args);
+	}
+}
